d4dd6017760689e5ab145959f7115bf7c92cc88a
Greg Jung <gvjung@gmail.com>
2018-11-06 10:40:24 -0800

    Reduce artifacts to install build, make check activated.   32-bit=GraphicsMagick + WxWidgets   64-bit=neither GM nor WX, mingw build.

--- scripts/ps1/make_gdl.ps1
+++ scripts/ps1/make_gdl.ps1
@@ -1,4 +1,9 @@
-  echo $env:WXNAME $env:GMNAME
+  # powershell script called from appveyor.yml in the build_script: section
+  # This is needed because any write to stderr, which is used by gcc and cmake,
+  # will invoke a powershell error.  
+  # evidently, the appveyor PS environment is carried through:
+    echo $env:WXNAME $env:GMNAME
+  # those two env. variables determine how the build proceeds.
       if ( $env:platform -Match "mingw64630x8664") 
       {
       $env:WXNAME=""
